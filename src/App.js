360a410f2d5e0194807bf439ca82435105d3844b
mr-git-gud <gitgudcasul@pm.me>
2019-08-13 23:48:00 +0100

    Times up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Title from './Components/Title';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/contracts/dc89ff49-8449-11e7-ac1d-3c52820efd20", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+      .then(response => response.json())
+      .then(data => this.setState({data: data}))
+  }
+
+
+  render(){
+    if(this.state.data !== null && this.state.data.attributes){
+      const { attributes } = this.state.data;
+      return(
+        <Title title={attributes.name}/>
+      )
+    } else {
+      return <Title title={""}/>
+    }
+  }
 }
 
 export default App;
